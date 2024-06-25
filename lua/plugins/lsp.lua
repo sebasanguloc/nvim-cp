ce11f6b37434ee4c367f190ee07ba91bfe481621
sebasanguloc <sebasanguloc@gmail.com>
2024-06-25 11:49:15 -0500

    server-client lua added

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,5 +2,18 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
         "williamboman/mason.nvim",
+        "folke/neodev.nvim",
     },
+    config = function()
+        require("neodev").setup()
+        require("lspconfig").lua_ls.setup({
+            settings = {
+                Lua = {
+                    telemetry = { enable = false },
+                    workspace = { checkThirdParty = false },
+                }
+            }
+        })
+        require("lspconfig").clangd.setup({})
+    end
 }
