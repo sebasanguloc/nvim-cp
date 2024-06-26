d46817c53a904180444d5d86e174e7eec3fdeee0
sebasanguloc <sebasanguloc@gmail.com>
2024-06-26 00:23:27 -0500

    correction compiler c++

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,28 +6,3 @@ map('n', '<leader>m', ':NvimTreeToggle<CR>')
 
 
 
--- Keymap para ejecutar el archivo C++ en Alacritty
-vim.api.nvim_set_keymap('n', '<leader>r', ':lua RunCppCodeInAlacritty()<CR>', { noremap = true, silent = true })
-
--- Función para compilar y ejecutar el archivo C++
-function _G.RunCppCodeInAlacritty()
-    -- Guardar el archivo actual
-    vim.cmd('write')
-
-    -- Obtener el nombre del archivo actual sin extensión
-    local file = vim.fn.expand('%:p')
-    local filename = vim.fn.expand('%:r')
-
-    -- Comando para compilar el archivo con clang
-    local compile_command = 'clang++ -std=c++20 -O2 -Wall ' .. file .. ' -o ' .. filename
-
-    -- Comando para ejecutar el archivo compilado en Alacritty
-    local run_command = 'alacritty -e sh -c "./' .. filename .. '; echo; read -p \'Presiona ENTER para continuar...\'; exit"'
-
-    -- Ejecutar el comando de compilación
-    os.execute(compile_command)
-
-    -- Ejecutar el comando para abrir Alacritty y ejecutar el archivo
-    os.execute(run_command)
-end
-
